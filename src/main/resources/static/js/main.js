1ed3d47f06b4591f0eeb9100cea19d2d3f8a28e1
KUN <yoonkun1902@gmail.com>
2024-08-30 14:43:27 +0900

    회원가입 완료 로그인까지 확인

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+var doc = window.document;
+doc.addEventListener('DOMContentLoaded', function(){
+    javascriptFun.initHtml();
+});
+
+
+var javascriptFun = {
+
+    initHtml: function () {
+
+
+    },
+
+}
+
+
